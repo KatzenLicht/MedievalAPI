7017b989605b001e3efb998345a094f4917af016
KatzenLicht <laaj12152a56@gmail.com>
2026-03-18 14:36:58 -0600

    Se arreglaron metodos API, falta DELETE

--- MedievalAPI/medievalapi.js
+++ MedievalAPI/medievalapi.js
@@ -1,107 +1,103 @@
 const http = require("node:http");
 const puerto = 3000;
 const fs = require("node:fs");
-
 var tarjetas;
-fs.readFile("./json/tarjetas.json", (err, archivo) => {
-    if(!err) {
-        tarjetas = JSON.parse(archivo.toString());
-    }
+fs.readFile("./json/tarjetas.json", "utf-8", (error, archivo) => {              
+            if(!error) {
+            tarjetas = JSON.parse(archivo);
+            }
+ 
 });
-
+ 
 const server = http.createServer((request, response) => {
-
     response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-
-    switch (request.method){
+    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+ 
+    switch(request.method) {
         case "GET":
             response.statusCode = 200;
             response.setHeader("Content-Type", "application/json");
             response.end(JSON.stringify(tarjetas));
-        break;
+            break;
         case "POST":
             var informacion = "";
             request.on("data", chunk => {
                 informacion += chunk;
             });
+ 
             request.on("end", () => {
                 const objeto_nueva_tarjeta = JSON.parse(informacion.toString());
                 tarjetas.arreglo.push(objeto_nueva_tarjeta);
-
+               
                 fs.writeFile("./json/tarjetas.json", JSON.stringify(tarjetas), err => {
                     if(!err) {
-                        response.statusCode = 201;
-                        response.setHeader("Content-Type", "application/json");
+                    response.statusCode = 201;
+                    response.setHeader("Content-Type", "application/json");
                         const objeto_respuesta = {
-                            mensaje: "Item guardado satisfactoriamente"
+                            mensaje: "Tarjeta creada",
                         }
-                        response.end(JSON.stringify(objeto_respuesta));
+ 
+                    response.end(JSON.stringify(objeto_respuesta));
                     }
                 });
-
-
-            })
-        break;
+            });
+            break;
         case "PUT":
             var informacion = "";
             request.on("data", chunk => {
                 informacion += chunk;
             });
-
+ 
             request.on("end", () => {
                 const objeto_tarjeta_modificada = JSON.parse(informacion);
-                
+ 
                 const objeto_modificado = {
                     titulo: objeto_tarjeta_modificada.titulo,
                     descripcion: objeto_tarjeta_modificada.descripcion,
                     imagen: objeto_tarjeta_modificada.imagen
                 }
-
+ 
                 tarjetas.arreglo[objeto_tarjeta_modificada.indice] = objeto_modificado;
-
+               
                 fs.writeFile("./json/tarjetas.json", JSON.stringify(tarjetas), err => {
                     if(!err) {
-                        response.statusCode = 200;
-                        response.setHeader("Content-Type", "application/json");
-                        const objeto_respuesta = {
-                            mensaje: "Elemento modificado correctamente"
-                        }
-                        response.end(JSON.stringify(objeto_respuesta));
+                    response.statusCode = 200;
+                    response.setHeader("Content-Type", "application/json");
+                    const objeto_respuesta = {
+                        mensaje: "Tarjeta modificada"
+                    }
+                    response.end(JSON.stringify(objeto_respuesta));
                     }
                 });
+ 
             });
-
-        break;
+            break;
         case "DELETE":
             request.on("data", info => {
-                const objeto_borrar = JSON.parse(info);
-                tarjetas.arreglo.splice(objeto_borrar.indice, 1);
-
-                fs.writeFile("./json/tarjetas.json", JSON.stringify(tarjetas), err => {
-                    if(!err) {
-                        response.statusCode = 200;
-                        response.setHeader("Content-Type", "application/json");
-                        const objeto_respuesta = {
-                            mensaje: "Elemento borrado correctamente"
-                        }
-                        response.end(JSON.stringify(objeto_respuesta));
-                    }
-                });
-            });
-
-        break;
+                const objeto_borrar = JSON.parse(info.toString());
+               tarjetas.arreglo.splice(objeto_borrar.indice, 1);
+ 
+               fs.writeFile("./json/tarjetas.json", JSON.stringify(tarjetas), err => {
+                if(!err) {
+                response.statusCode = 204;
+                response.setHeader("Content-Type", "application/json");
+                const objeto_respuesta = {
+                    mensaje: "Tarjeta borrada"
+                }
+                response.end(JSON.stringify(objeto_respuesta));
+                }
+               });
+           
+            })
+            break;
         case "OPTIONS":
+            response.statusCode = 204;
             response.writeHead(204);
             response.end();
-        break;
+            break;
     }
-
-
-
-
 });
-
+ 
 server.listen(3000, () => {
     console.log("Servidor a la escucha en http://localhost:" + puerto);
 });
--- PaginaMedieval/js/conectar_api.js
+++ PaginaMedieval/js/conectar_api.js
@@ -1,126 +1,147 @@
-const plantilla_tarjeta = document.querySelector(".ContenedorItem");
-const ColeccionItems = document.querySelector("#ColeccionItems");
-const PantallaAgregarItem = document.querySelector(".PantallaAgregarItem");
-
-plantilla_tarjeta.remove();
-
-fetch("http://localhost:3000").then(recurso => recurso.json()).then(datos => {
+const tarjeta = document.querySelector(".ContenedorItem");
+tarjeta.remove();
+ 
+const ColeccionItems= document.querySelector("#ColeccionItems");
+const pantallaAgregarItem = document.querySelector(".PantallaAgregarItem");
+ 
+ 
+fetch("http://localhost:3000").then(recurso => recurso.json())
+.then(datos => {
+   
     console.log(datos);
-
-    for (i = 0; i < datos.arreglo.length; i++){
-        const clon = plantilla_tarjeta.cloneNode(true);
+ 
+    for(i=0; i<datos.arreglo.length; i++){
+        const clon = tarjeta.cloneNode(true);
         ColeccionItems.appendChild(clon);
-
-        const Titulo = clon.querySelector(".Titulo");
-        Titulo.innerHTML = datos.arreglo[i].titulo;
-
-        const Descripcion = clon.querySelector(".Descripcion");
-        Descripcion.innerHTML = datos.arreglo[i].descripcion;
-        
-        const Imagen = clon.querySelector(".Imagen");
-        Imagen.style.backgroundImage = "url(" + datos.arreglo[i].imagen + ")";
-
+ 
+       
+        const titulo = clon.querySelector(".Titulo");
+        titulo.innerHTML = datos.arreglo[i].titulo;
+ 
+        const descripcion = clon.querySelector(".Descripcion");
+        descripcion.innerHTML = datos.arreglo[i].descripcion;
+ 
+        const imagen = clon.querySelector(".Imagen");
+        imagen.style.backgroundImage = "url('" + datos.arreglo[i].imagen + "')";
+ 
         const Icono_Borrar = clon.querySelector(".Icono_Borrar");
         Icono_Borrar.tag = i;
-        Icono_Borrar.addEventListener("click", (evento) => {
-            evento.currentTarget
+        Icono_Borrar.addEventListener("click", (evento)=>{
+           
             const objeto_borrar = {
                 indice: evento.currentTarget.tag
             }
-            
-            fetch("http://localhost:3000", {
+ 
+            fetch("http://localhost:3000/", {
                 method: "DELETE",
                 body: JSON.stringify(objeto_borrar)
-            }).then(recurso => recurso.json()).then(respuesta => {
-                alert(respuesta.mensaje);
-                window.location.reload();
             })
-        });
-        const PanelCreacionItem = clon.querySelector(".PanelCreacionItem")
-
-        const CrearItem_InputTitulo = PanelCreacionItem.querySelector(".CrearItem_InputTitulo");
-        const input_descripcion = PanelCreacionItem.querySelector("textarea");
-        const ImagenPreview = PanelCreacionItem.querySelector(".ImagenPreview");
-
-        const Icono_Editar = clon.querySelector(".Icono_editar");
-        Icono_Editar.addEventoListener("click", function(){
-            PanelCreacionItem.style.display = "flex";
-            CrearItem_InputTitulo.value = Titulo.innerHTML;
-            input_descripcion.value = Descripcion.innerHTML;
-            ImagenPreview.src = Imagen.style.backgroundImage.split('""')[1];
-        });
-
-        const input_imagen = PanelCreacionItem.querySelector(".input_imagen");
-        input_imagen.addEventListener("change", () => {
-        const reader = new FileReader();
-        reader.readAsDataURL(input_imagen.files[0]);
-            reader.onload = () => {
-                ImagenPreview.src = reader.result;
-            }
-        });
-        const boton_guardar = PanelCreacionItem.querySelector(".boton_guardar");
-        boton_guardar.addEventListener("click", function {
-            const objeto_info_tarjeta_modificada = {
-                titulo: CrearItem_InputTitulo.value,
-                descripcion: input_descripcion.value,
-                imagen: ImagenPreview.src
-            }
-        
-            fetch("http://localhost:3000", {
-                method: "PUT",
-                body: JSON.stringify(objeto_info_tarjeta_modificada);
-            }).then(recurso => recurso.json()).then (respuesta => {
+            .then(recurso => recurso.json())
+            .then(respuesta => {
                 alert(respuesta.mensaje);
                 window.location.reload();
             });
         });
+ 
+        const PanelCreacionItem = clon.querySelector(".PanelCreacionItem");
+        const Icono_Editar = clon.querySelector(".Icono_Editar");
+        Icono_Editar.tag = i;
+        indice = i;
+ 
+        const CrearItem_InputTitulo = PanelCreacionItem.querySelector(".CrearItem_InputTitulo");
+        const input_descripcion = PanelCreacionItem.querySelector("textarea");
+        const input_imagen = PanelCreacionItem.querySelector(".input_imagen");
+        const ImagenPreview = PanelCreacionItem.querySelector(".ImagenPreview");
+ 
+        Icono_Editar.addEventListener("click", (evento)=>{
+            PanelCreacionItem.style.display = "flex";
+           
+            CrearItem_InputTitulo.value = datos.arreglo[evento.currentTarget.tag].titulo;
+            input_descripcion.value = datos.arreglo[evento.currentTarget.tag].descripcion;
+            input_imagen.value = "";
+            ImagenPreview.src = datos.arreglo[evento.currentTarget.tag].imagen;
+ 
+            const boton_cancelar = clon.querySelector(".boton_cancelar");
+            boton_cancelar.addEventListener("click", ()=>{
+                PanelCreacionItem.style.display = "none";
+            });
+ 
+            const boton_guardar = clon.querySelector(".boton_guardar");
+            boton_guardar.addEventListener("click", ()=>{
+                const objeto_info_tarjeta_modificada = {
+                    indice: indice,
+                    titulo: CrearItem_InputTitulo.value,
+                    descripcion: input_descripcion.value,
+                    imagen: ImagenPreview.src
+                }
+ 
+                fetch("http://localhost:3000/", {
+                    method: "PUT",
+                    body: JSON.stringify(objeto_info_tarjeta_modificada)
+                }).then(recurso => recurso.json())
+                .then(respuesta => {
+                    alert(respuesta.mensaje);
+                    window.location.reload();
+                });
+            });
+ 
+            input_imagen.addEventListener("change", ()=>{
+            const reader = new FileReader();
+            reader.readAsDataURL(input_imagen.files[0]);
+            reader.onload = ()=>{
+            ImagenPreview.src = reader.result;
+                }
+ 
+            });
+        });
+ 
     }
-    ColeccionItems.appendChild(PantallaAgregarItem);
+    ColeccionItems.appendChild(pantallaAgregarItem);
 });
-
-PantallaAgregarItem.addEventListener("click", () => {
-    const clon = plantilla_tarjeta.cloneNode(true);
+ 
+pantallaAgregarItem.addEventListener("click", ()=>{
+   
+    const clon = tarjeta.cloneNode(true);
     ColeccionItems.appendChild(clon);
-    ColeccionItems.appendChild(PantallaAgregarItem);
-
+ 
+    ColeccionItems.appendChild(pantallaAgregarItem);
+ 
     const PanelCreacionItem = clon.querySelector(".PanelCreacionItem");
     PanelCreacionItem.style.display = "flex";
-
+ 
+    //referencias de input
     const CrearItem_InputTitulo = PanelCreacionItem.querySelector(".CrearItem_InputTitulo");
-
     const input_descripcion = PanelCreacionItem.querySelector("textarea");
-
     const input_imagen = PanelCreacionItem.querySelector(".input_imagen");
     const ImagenPreview = PanelCreacionItem.querySelector(".ImagenPreview");
-
-    input_imagen.addEventListener("change", () => {
+ 
+    input_imagen.addEventListener("change", ()=>{
         const reader = new FileReader();
         reader.readAsDataURL(input_imagen.files[0]);
-        reader.onload = () => {
+        reader.onload = ()=>{
             ImagenPreview.src = reader.result;
         }
     });
-
+ 
     const boton_guardar = clon.querySelector(".boton_guardar");
-    boton_guardar.addEventListener("click", () => {
+    boton_guardar.addEventListener("click", ()=>{
         const objeto_info_nueva_tarjeta = {
             titulo: CrearItem_InputTitulo.value,
             descripcion: input_descripcion.value,
             imagen: ImagenPreview.src
-        }
-        console.log(objeto_info_nueva_tarjeta);
-
-        fetch("http://localhost:3000", {
+        };
+        fetch("http://localhost:3000/", {
             method: "POST",
             body: JSON.stringify(objeto_info_nueva_tarjeta)
-        }).then(recurso => recurso.json()).then(respuesta => {
+        }).then(recurso => recurso.json())
+        .then(respuesta => {
             alert(respuesta.mensaje);
             window.location.reload();
         });
     });
-
     const boton_cancelar = clon.querySelector(".boton_cancelar");
-    boton_cancelar.addEventListener("click", () => {
+    boton_cancelar.addEventListener("click", ()=>{
         clon.remove();
     });
+ 
 });
